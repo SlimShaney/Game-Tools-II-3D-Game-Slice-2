dd5bcb28ccd41d4d7331b68c278b2775ef5ff0cc
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2018-12-30 13:01:36 +0000

    Commit 1

---
+++ Game Tools II - Assignment 2/Assets/Spell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour {
+    [SerializeField] GameObject spellEffect;
+    [SerializeField] Transform effectReference;
+    [SerializeField] Character player;
+
+    private void OnEnable()
+    {
+        player.OnFire.AddListener(Fire);
+    }
+
+    private void OnDisable()
+    {
+        player.OnFire.RemoveListener(Fire);
+    }
+
+    public void Fire()
+    {
+        Instantiate(spellEffect, effectReference.position, effectReference.rotation);
+    }
+}
